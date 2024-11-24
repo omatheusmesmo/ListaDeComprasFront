f3f43614d594c51d9a7d8adfbcf878a2f861e294
omatheusmesmo <matheus.6148@gmail.com>
2024-11-24 16:51:02 -0300

    lista-itens

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterOutlet } from '@angular/router';  // Importando o RouterOutlet para navegação
+import { RouterModule } from '@angular/router';  // Importando o RouterModule para o roteamento
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';  // Importando o toggle
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [MatSlideToggleModule, RouterModule, RouterOutlet],  // Importando ambos corretamente
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'lista-de-compras';
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,18 @@
 import { Routes } from '@angular/router';
+import { ListaItensComponent } from './lista-itens/lista-itens.component';
+import { FormularioItemComponent } from './formulario-item/formulario-item.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: ListaItensComponent },
+  { path: 'adicionar', component: FormularioItemComponent },
+  { path: 'editar/:id', component: FormularioItemComponent,
+  //  getPrerenderParams: () => {
+  //    // Você pode fornecer uma lista de IDs mockados ou //pegar dados da sua API
+  //    return [
+   //     { id: 1 },
+  //      { id: 2 },
+  //      { id: 3 }
+  //    ];
+  //  }
+  }
+];
