d9b409e906881c95153891ed44c74277c5dbd316
omatheusmesmo <matheus.6148@gmail.com>
2024-11-28 04:59:25 -0300

    tela de adição de itens e ajustes na UI para mais consistência

--- src/app/formulario-item/formulario-item.component.ts
+++ src/app/formulario-item/formulario-item.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-formulario-item',
   standalone: true,
-  imports: [CommonModule],  // Importar módulos necessários, como CommonModule
+  imports: [
+    CommonModule,
+    MatFormFieldModule, // Importação para usar mat-form-field
+    MatInputModule,     // Importação para usar mat-input
+    MatButtonModule,    // Importação para usar botões do Angular Material
+  ],
   templateUrl: './formulario-item.component.html',
   styleUrls: ['./formulario-item.component.scss']
 })
